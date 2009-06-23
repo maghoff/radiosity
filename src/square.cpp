a1f7a5291cc72e4c72509b05a2b873ed4cf8f975
Unknown <mag@fabrikken>
2009-06-24 00:46:40 +0200

    Made the sides of the square into objects with double textures

---
+++ src/square.cpp
@@ -0,0 +1,63 @@
+#include <GL/gl.h>
+#include "gl_double_texture.hpp"
+#include "square.hpp"
+
+struct square::impl {
+	float r, g, b;
+	float ox, oy, oz;
+	float tdx, tdy, tdz;
+	float udx, udy, udz;
+
+	gl_double_texture tex;
+
+	void vertex(float t, float u);
+};
+
+void square::impl::vertex(float t, float u) {
+	glTexCoord2f(t, u);
+	glVertex3f(ox + t*tdx + u*udx, oy + t*tdy + u*udy, oz + t*tdz + u*udz);
+}
+
+square::square() :
+	d(new impl)
+{
+}
+
+square::~square() {
+}
+
+void square::set_color(float r, float g, float b) {
+	d->r = r; d->g = g; d->b = b;
+}
+
+void square::set_origin(float x, float y, float z) {
+	d->ox=x; d->oy=y; d->oz=z;
+}
+
+void square::set_t_direction(float x, float y, float z) {
+	d->tdx=x; d->tdy=y; d->tdz=z;
+}
+
+void square::set_u_direction(float x, float y, float z) {
+	d->udx=x; d->udy=y; d->udz=z;
+}
+
+void square::render() {
+	glBindTexture(GL_TEXTURE_2D, d->tex.front_id());
+
+	glBegin(GL_QUADS);
+
+	glColor4f(d->r, d->g, d->b, 1);
+	d->vertex(0, 0);
+	d->vertex(1, 0);
+	d->vertex(1, 1);
+	d->vertex(0, 1);
+
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+gl_double_texture& square::tex() {
+	return d->tex;
+}
---
+++ src/square.hpp
@@ -0,0 +1,27 @@
+#ifndef SIMPLEMODEL_SQUARE_HPP
+#define SIMPLEMODEL_SQUARE_HPP
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/noncopyable.hpp>
+
+class gl_double_texture;
+
+class square : public boost::noncopyable {
+	struct impl;
+	boost::scoped_ptr<impl> d;
+
+public:
+	square();
+	~square();
+
+	void set_color(float, float, float);
+	void set_origin(float, float, float);
+	void set_t_direction(float, float, float);
+	void set_u_direction(float, float, float);
+
+	void render();
+
+	gl_double_texture& tex();
+};
+
+#endif
