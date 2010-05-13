b67555eae4907c0489f85d59fd6278406dd62664
maghoff <unknown>
2010-05-14 00:15:52 +0200

    Enabled multiple build variants

---
+++ GCCSetup.cmake
@@ -0,0 +1,7 @@
+# -D_GLIBCXX_DEBUG makes it crash :(
+
+# Try to re-enable -Werror!
+
+SET(CMAKE_CXX_FLAGS_DEBUG "-ggdb -DEBUG -DDEBUG -ffast-math -pipe -Wall" CACHE STRING "LOL" FORCE)
+SET(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -ffast-math -pipe -Wall -Wl,-s" CACHE STRING "LOL" FORCE)
+
--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install libcwiid1-dev libboost-thread-dev
+PACKAGES="libcwiid1-dev libboost-thread-dev"
+CMAKE_PARAMS="-C../GCCSetup.cmake -DCMAKE_INSTALL_PREFIX=/usr"
+BUILD_TYPES="debug release"
 
-echo
-echo "Remember to install Ymse"
+
+if ! dpkg -l $PACKAGES >/dev/null
+then
+    echo "Will install packages: $PACKAGES"
+    sudo apt-get install $PACKAGES
+fi
+
+for x in $BUILD_TYPES
+do
+    mkdir -p "$x"
+
+    pushd "$x"
+    cmake $CMAKE_PARAMS "-DCMAKE_BUILD_TYPE=$x" ..
+    popd
+done
